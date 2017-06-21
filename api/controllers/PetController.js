a3e8bf27ad177a48f6a7376508112db1276afdf0
David Figueroa <davidfigueroar9@gmail.com>
2017-06-21 14:38:49 -0500

    pet controller

--- api/controllers/PetController.js
+++ api/controllers/PetController.js
@@ -6,25 +6,5 @@
  */
 
 module.exports = {
-	find: function (req,res) {
-		if (req.user.role.slug == 'client') {
-			Pet.find({owner : req.user.id})
-			.populate(['size', 'race', 'photos', 'avatar'])
-			.then(pets => {
-				return res.json(200, pets);
-			})
-			.catch(err => {
-				return res.json(err.status, {err: err});
-			});
-		} else {
-			Pet.find()
-			.populate(['size', 'race', 'photos'])
-			.then(pets => {
-				return res.json(200, pets);
-			})
-			.catch(err => {
-				return res.json(err.status, {err: err});
-			});
-		}
-	}
+	
 };
